1923c4f6c9c7f56acbaae9c642132cddfc3ae5bf
Kirill Feoktistov <fekirill@gmail.com>
2014-06-15 17:56:59 +0400

    Descriptions for functions of assignment #1 added

--- Assignment1/corr.R
+++ Assignment1/corr.R
@@ -1,3 +1,11 @@
+## Write a function that takes a directory of data files and a threshold
+## for complete cases and calculates the correlation between sulfate and
+## nitrate for monitor locations where the number of completely observed
+## cases (on all variables) is greater than the threshold. The function
+## should return a vector of correlations for the monitors that meet the
+## threshold requirement. If no monitors meet the threshold requirement,
+## then the function should return a numeric vector of length 0.
+
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
